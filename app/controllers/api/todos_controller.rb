5942cc8f41fed3e84a71b04981fa879aeed05da0
Rohit sharma <rosharma@watermarkinsights.com>
2023-08-23 12:19:16 +0530

    feature - TODO CRUD operations added

---
+++ app/controllers/api/todos_controller.rb
@@ -0,0 +1,56 @@
+class Api::TodosController < ApplicationController
+  def create
+    tr= Todo.create(title: params[:title],description: params[:description],tags: params[:tags]) 
+    render json: tr 
+  end
+  
+  def update
+    tr = Todo.find_by(id: params[:id])
+    if tr
+      tr.update(title: params[:title],description: params[:description],tags: params[:tags])
+      render json: tr 
+    else
+      render json: {
+        message:"id you enter does not exist"
+      }
+    end
+  end
+
+  def index
+    @tr = Todo.all
+    render json: @tr
+  end
+
+  def show
+    tr = Todo.find_by(id: params[:id])
+    if tr 
+      render json: tr
+    else
+      render json: {
+        error: "Not found"
+        }
+    end
+  end
+
+  def destroy
+    todo = Todo.find(params[:id])
+    todo.destroy    
+      render json: {
+        message: "destroyed"
+        }
+  end
+
+  def edit
+  end
+
+  def new
+  end
+  private 
+  def anti_params
+    params.require(:todo).permit([
+      :title,
+      :description,
+      :tags
+    ])
+  end
+end
